5fcbe1dd144c14841a0474ed482dfdb04faa31a5
Jerry Kiely <jerry@cowboysmall.com>
2015-01-01 12:19:52 -0500

    migrated to python 3 - setup.py changed to python 3 only

--- setup.py
+++ setup.py
@@ -32,11 +32,7 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3 :: Only',
     ],
 
     test_suite='tests',
